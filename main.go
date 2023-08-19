4366b15d0c381b4dff5ab22a79a43edbf5b860df
Corey Burns <coreyburns@outlook.com>
2023-08-19 17:33:08 -0400

    added error checking on the routes (basic), can only post on post, etc

--- main.go
+++ main.go
@@ -6,29 +6,35 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from corey webserver"))
 }
 
 func showPost(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Display a specific snippet here."))
 }
+
 func createPost(w http.ResponseWriter, r *http.Request) {
+	const methodNotAllowedError = 405
+
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		// replaces w.WriteHeader(405) and w.Write([]byte("Method not allowed"))
+		http.Error(w, "Method Not Allowed", methodNotAllowedError)
+		return
+	}
 	w.Write([]byte("Create a new post"))
 }
 
 func main() {
-	// initialize new server
 	mux := http.NewServeMux()
 
-	// pass home function to handle requests to the site "/"
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/post", showPost)
 	mux.HandleFunc("/post/create", createPost)
-
-	// Use the http.ListenAndServe() function to start a new web server. We pas
-	// two parameters: the TCP network address to listen on (in this case ":4000
-	// and the servemux we just created. If http.ListenAndServe() returns an er
-	// we use the log.Fatal() function to log the error message and exit.
 
 	log.Println("Starting server on :4002")
 	err := http.ListenAndServe(":4002", mux)
